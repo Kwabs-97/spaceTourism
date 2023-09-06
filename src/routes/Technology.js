7c5461e19b706e84776d03743f5586e0527b3e07
Kwabs-97 <khobewayne@gmail.com>
2023-09-06 10:27:40 +0000

    tech laptop screen size

--- src/routes/Technology.js
+++ src/routes/Technology.js
@@ -20,7 +20,7 @@ function Technology() {
       title: "LAUNCH VEHICLE",
       description:
         "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-      img: launchVehicleIMG,
+      img: launchVehiclePortrait,
       imgPortrait: launchVehiclePortrait,
     },
 
